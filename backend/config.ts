d466d204e88db2fee227df477c67630318a0e474
Artem Markelov <artem77timoha77@gmail.com>
2023-06-01 14:53:22 +0600

    #job - Создал базовый проект

---
+++ backend/config.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const rootPath = __dirname;
+
+const config = {
+  rootPath,
+  publicPath: path.join(rootPath, 'public'),
+  db: 'mongodb://localhost/job',
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+  },
+  mail: 'esdpjs17@gmail.com',
+  site: 'http://localhost:3000',
+};
+
+export default config;
